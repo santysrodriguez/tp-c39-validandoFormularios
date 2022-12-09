a111af587a7d0bea4732ef7da35020ad96e440c7
santysrodriguez <santiagocarp987@gmail.com>
2022-12-09 15:10:12 -0300

    tp terminado

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -1,3 +1,5 @@
+const $ = (elemento) =>  document.getElementById(elemento)
+
 window.onload = function () {
     let titulo = document.querySelector('.moviesAddTitulo')
     let formulario = document.querySelector('#formulario');
@@ -10,59 +12,59 @@ window.onload = function () {
     //------DESDE AQUÍ CONTINÚE CON LAS VALIDACIONES DEL FORMULARIO //
     //-------------------DE REGISTRO DE PELÍCULAS------------------//    
 
-    document.getElementById("title").focus()
+    $("title").focus()
 
     let inputs = document.querySelectorAll("#formMoviesAdd input")
     inputs = [...inputs, document.querySelector("#formMoviesAdd select")]
 
 
-    const validarCampo = (target, campo, msg, valEsp) => {
-        if (target.value === "" || valEsp) {
-            document.getElementById(`small-${campo}`).innerHTML = `${msg}`
-            document.getElementById(`small-${campo}`).classList.add("is-invalidText")
-            document.getElementById(`${campo}`).classList.add("is-invalidBorder")
-            document.getElementById(`${campo}`).classList.remove("is-validBorder")
+    const validacionCampo = (target, campo, message, valEspecial) => {
+        if (target.value === "" || valEspecial) {
+            $(`span-${campo}`).innerHTML = `${message}`
+            $(`span-${campo}`).classList.add("is-invalidText")
+            $(`${campo}`).classList.add("is-invalidBorder")
+            $(`${campo}`).classList.remove("is-validBorder")
         } else {
-            document.getElementById(`small-${campo}`).innerHTML = ""
-            document.getElementById(`small-${campo}`).classList.remove("is-invalidText")
-            document.getElementById(`${campo}`).classList.remove("is-invalidBorder")
-            document.getElementById(`${campo}`).classList.add("is-validBorder")
+            $(`span-${campo}`).innerHTML = ""
+            $(`span-${campo}`).classList.remove("is-invalidText")
+            $(`${campo}`).classList.remove("is-invalidBorder")
+            $(`${campo}`).classList.add("is-validBorder")
         }
     }
 
 
-    const validarFormulario = (e) => {
+    const validacionForm = (e) => {
         switch (e.target.name) {
             case "title":
-                validarCampo(e.target, "title", "Debes completar este campo")
+                validacionCampo(e.target, "title", "Debes completar este campo")
                 break;
             case "rating":
                 if (+e.target.value < 0 || +e.target.value > 10) {
-                    validarCampo(e.target, "rating", "Entre 0 y 10", true)
+                    validacionCampo(e.target, "rating", "Entre 0 y 10", true)
                 }else{
-                    validarCampo(e.target, "rating", "Debes completar este campo")
+                    validacionCampo(e.target, "rating", "Debes completar este campo")
                 }
                 
                 break;
             case "awards":
                 if (+e.target.value < 0 || +e.target.value > 10) {
-                    validarCampo(e.target, "awards", "Entre 0 y 10", true)
+                    validacionCampo(e.target, "awards", "Entre 0 y 10", true)
                 }else{
-                    validarCampo(e.target, "awards", "Debes completar este campo")
+                    validacionCampo(e.target, "awards", "Debes completar este campo")
                 }
                 break;
             case "release_date":
-                validarCampo(e.target, "release_date", "Debes completar este campo")
+                validacionCampo(e.target, "release_date", "Debes completar este campo")
                 break;
             case "length":
                 if (+e.target.value < 60 || +e.target.value > 360) {
-                    validarCampo(e.target, "length", "Entre 60 y 360 minutos", true)
+                    validacionCampo(e.target, "length", "Entre 60 y 360 minutos", true)
                 }else{
-                    validarCampo(e.target, "length", "Debes completar este campo")
+                    validacionCampo(e.target, "length", "Debes completar este campo")
                 }
                 break;
             case "genre_id":
-                validarCampo(e.target, "genre_id", "Debes completar este campo")
+                validacionCampo(e.target, "genre_id", "Debes completar este campo")
                 break;
             default:
                 break;
@@ -70,24 +72,24 @@ window.onload = function () {
     }
 
     inputs.forEach(input => {
-        input.addEventListener("keyup", validarFormulario)
-        input.addEventListener("blur", validarFormulario)
+        input.addEventListener("keyup", validacionForm)
+        input.addEventListener("blur", validacionForm)
     });
 
-    document.getElementById("formMoviesAdd").addEventListener("submit", (e) =>{
-        let ulErrores = document.querySelector(".errores")
+    $("formMoviesAdd").addEventListener("submit", (e) =>{
+        let erroresUl = document.querySelector(".errores")
         let errores = []
-        for (let x = 0; x < 6; x++) {
-            if (e.target[x].value === "" || e.target[x].classList.contains("is-invalidBorder")) {
-                if (e.target[x].value === "") {
-                    validarCampo(e.target[x], e.target[x].name, "Debes completar este campo")
-                    errores.push( `${e.target[x].name}: Debes completar este campo`)
-                } else if (e.target[x].name === "length") {
-                    validarCampo(e.target[x], e.target[x].name, "Entre 60 y 360 minutos", true)
-                    errores.push( `${e.target[x].name}: Entre 60 y 360 minutos`)
-                } else if (e.target[x].name === "rating" || e.target[x].name === "awards"){
-                    validarCampo(e.target[x], e.target[x].name, `Entre 0 y 10`, true)
-                    errores.push( `${e.target[x].name}: Entre 0 y 10`)
+        for (let i = 0; i < 6; i++) {
+            if (e.target[i].value === "" || e.target[i].classList.contains("is-invalidBorder")) {
+                if (e.target[i].value === "") {
+                    validacionCampo(e.target[i], e.target[i].name, "Debes completar este campo")
+                    errores.push( `${e.target[i].name}: Debes completar este campo`)
+                } else if (e.target[i].name === "length") {
+                    validacionCampo(e.target[i], e.target[i].name, "Entre 60 y 360 minutos", true)
+                    errores.push( `${e.target[i].name}: Entre 60 y 360 minutos`)
+                } else if (e.target[i].name === "rating" || e.target[i].name === "awards"){
+                    validacionCampo(e.target[i], e.target[i].name, `Entre 0 y 10`, true)
+                    errores.push( `${e.target[i].name}: Entre 0 y 10`)
                 }
             }
         }
@@ -95,18 +97,18 @@ window.onload = function () {
         if (errores.length > 0) {
             e.preventDefault()
             
-            ulErrores.classList.add("alert-warning")
-            ulErrores.innerHTML = ""
+            erroresUl.classList.add("alert-warning")
+            erroresUl.innerHTML = ""
             errores.forEach(error => {
-                ulErrores.innerHTML += `<li>${error}</li>`
+                erroresUl.innerHTML += `<li>${error}</li>`
             });
             setTimeout(() => {
-                ulErrores.innerHTML = ""
-            }, 5000)
+                erroresUl.innerHTML = ""
+            }, 10000)
         }else{
-            ulErrores.classList.remove("alert-warning")
-            ulErrores.classList.add("alert-exito")
-            ulErrores.innerHTML = "La película se guardó satisfactoriamente."
+            erroresUl.classList.remove("alert-warning")
+            erroresUl.classList.add("alert-exito")
+            erroresUl.innerHTML = "La película se guardó satisfactoriamente."
         }
     })
 }
